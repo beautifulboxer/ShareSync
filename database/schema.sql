78cccaec39e862c3bec4fac6c95fb2d6e9119acf
Vaneesh Jain <vaneeshjain91@gmail.com>
2026-09-18 19:59:22 +0530

    added usertable

--- database/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS sharesync;
-USE sharesync;
-
-CREATE TABLE users (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL
-);
