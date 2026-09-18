500b6bdd212634dd1171cce2a4152207459b8d06
Vaneesh Jain <vaneeshjain91@gmail.com>
2026-09-18 20:04:03 +0530

    added table users

---
+++ database/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS sharesync;
+USE sharesync;
+
+create table users(
+userid INT primary key auto_increment,
+name varchar(100) NOT NULL,
+email varchar(100) UNIQUE NOT NULL,
+password varchar(255) NOT NULL
+);
