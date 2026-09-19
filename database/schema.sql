e8e7d0dd73ed52dd9575b1f74a9aacd2d76b5dd1
tawana chi <tawanachikwanda12@gmail.com>
2026-09-19 18:24:53 +0530

    made changes

--- database/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS sharesync;
-USE sharesync;
-
-create table users(
-userid INT primary key auto_increment,
-name varchar(100) NOT NULL,
-email varchar(100) UNIQUE NOT NULL,
-password varchar(260) NOT NULL
-);
