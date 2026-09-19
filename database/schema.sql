78760707c7c6bbc5f0e944a108042d887de1c1d3
Vaneesh Jain <vaneeshjain91@gmail.com>
2026-09-19 13:52:59 +0530

    Updated password length

--- database/schema.sql
+++ database/schema.sql
@@ -5,5 +5,5 @@ create table users(
 userid INT primary key auto_increment,
 name varchar(100) NOT NULL,
 email varchar(100) UNIQUE NOT NULL,
-password varchar(255) NOT NULL
+password varchar(260) NOT NULL
 );
